75748f5fa41a263ee4bff4eaaa0b49f306bcf53a
Philip Bollen <flip.philip@hotmail.com>
2021-10-12 10:33:42 +0200

    Refactor code

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 ## Opdracht informatie
 Opdrachtgever: Jim Bemelen  
 Docent: Marcel Schmitz
+
+# TO-DO list
+- Spreken met de andere groep
+- Afronden en inpakken (zorgen dat er een installatie plan is)
+- Fysieke hulpmiddelen
--- boltCode.js
+++ boltCode.js
@@ -1,37 +1,10 @@
 // Program constants
 /** The preset link to the server */
-const serverLink = "https://rollenbollen.azurewebsites.net/";
+const serverLink = 'https://rollenbollen.azurewebsites.net/';
 /** The preset link to the api on the server */
-const apiLink = serverLink + "api/";
-const MIN_SPEED = 5.0;
-const minSpeedSq = MIN_SPEED ** 2;
+const apiLink = `${serverLink}api/`;
 
-const floorPlan = [
-  [
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-  ],
-  [
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-  ],
-  [
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-  ],
-  [
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-    { n: 0, ne: 0, e: 0, se: 0, s: 0, sw: 0, w: 0, nw: 0 },
-  ],
-];
+
 const speedOffset = 1;
 registerMatrixAnimation({
   frames: [
@@ -346,8 +319,8 @@ registerMatrixAnimation({
  * @param {int} adjacent The adjacent side of the triangle.
  * @return {int} The precise angle which the BOLT has to turn
  */
-function calcAngle(opposite, adjacent) {
-  if (0 == adjacent) {
+const calcAngle = (opposite, adjacent) => {
+  if (0 === adjacent) {
     if (opposite > 0) {
       return 90;
     } else {
@@ -363,17 +336,18 @@ function calcAngle(opposite, adjacent) {
   }
   angle = angle % 360;
   return angle;
-}
+};
 /**
  * calculate the distance between two points, in a 2D plain.
  * @param {int} opposite The opposite side of the triangle.
  * @param {int} adjacent The adjacent side of the triangle.
  * @return {int} total distance via the pythagorean theorem.
  */
-function calcDistance(opposite, adjacent) {
-  return Math.sqrt(opposite ** 2 + adjacent ** 2);
-}
-const boltIdLink = () => apiLink + "bolt/" + boltId + "/";
+const calcDistance = (opposite, adjacent) => (
+  Math.sqrt(opposite ** 2 + adjacent ** 2)
+)
+
+const boltIdLink = () => `${apiLink}bolt/${boltId}/`;
 
 // User classes
 /**
@@ -388,13 +362,6 @@ class Bolt {
       x: 0,
       y: 0,
     };
-  }
-  /**
-   * Check if a move is possible
-   * @return {boolean}, it's possible or it isn't
-   */
-  possibleMoves() {
-    return floorPlan[this.position.y][this.position.x];
   }
   /**
    * Drive the BOLT to the given point in space from the current space.
@@ -445,27 +412,26 @@ async function startProgram() {
 /**
  * Make a call to the register API interface
  */
-async function registerBolt() {
-  const response = await fetch(apiLink + "register");
+const registerBolt = async () => {
+  const response = await fetch(apiLink + 'register');
   const id = await response.json();
   boltId = id;
-  await speak("Bolt id is " + boltId);
-}
+  await speak('Bolt id is ' + boltId);
+};
 /**
  * Make a call to the web-API for the next move
  */
-async function getNextMove() {
-  const response = await fetch(boltIdLink() + "command");
+const getNextMove = async () => {
+  const response = await fetch(boltIdLink() + 'command');
   const move = await response.json();
-  const x = move.x;
-  const y = move.y;
+  const { x, y } = move;
   await bolt.drive(x, y);
-  if (-1 == x && -1 == y) {
+  if (-1 === x && -1 === y) {
     running = false;
   }
-}
+};
 
 // Register events
 registerEvent(EventType.onCollision, async () => {
-  await speak("Oof!");
+  await speak('Oof!');
 });
